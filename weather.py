57c23e33b11e3a280b217c68425b9ff4e05ccef9
M S Darshan Kirthic <97836674+IamSmokeY@users.noreply.github.com>
2022-12-24 19:22:59 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,6 +1,7 @@
 
 import requests
 from datetime import datetime as dt
+
 apikey = "---your api goes heere---"
 baseurl = "http://api.openweathermap.org/data/2.5/weather"
 
@@ -9,8 +10,8 @@ city = input("Enter the city: ")
 url = f"{baseurl}?appid={apikey}&q={city}"
 #print("URL= {}".format(url))  #To print the URL
 
-response = requests.get(url)
-if response.status_code!=200:
+response = requests.get(url)  
+if response.status_code!=200:         #if reponse status is not 200 it exits the program
     print("\033[1;31;40mERROR {}{}".format(response.status_code,"\033[0;37;0m "))
     exit()
 
